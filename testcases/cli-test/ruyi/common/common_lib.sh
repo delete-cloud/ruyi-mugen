292b1c483c9ad6229f397d8dd7e981aa7e5b994d
weilinfox <caiweilin@iscas.ac.cn>
2024-07-08 20:43:55 +0800

    Test 0.14.0-beta.20240708
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- testcases/cli-test/ruyi/common/common_lib.sh
+++ testcases/cli-test/ruyi/common/common_lib.sh
@@ -94,7 +94,7 @@ export_release_ruyi_link() {
 }
 
 export_github_release_ruyi_link() {
-	version="0.14.0-beta.20240707"
+	version="0.14.0-beta.20240708"
 	arch='amd64'
 	local larch="$(uname -m)"
 	if [ "$larch"  == "riscv64" ]; then arch='riscv64'; fi
